026c9c1fb20786f42e4a3c8149c864dfc0649d29
Sedar Yildirim <40918409+sedaryildirim@users.noreply.github.com>
2021-11-29 22:40:31 +0000

    JS lessons 0 to 10
    lessons 0 to 10 added

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// Lesson 1 
+
+// pop up on page load that says hello world
+// alert('hello world');
+
+// logs 1, 2, 3 to console in developer tools
+console.log(1); 
+console.log(2); 
+console.log(3); 
+
+// variables
+// variables store value to be reused later
+let age = 25;
+let year = 2019;
+
+// let age (variable) = 25
+// let year (variable) = 2019
+// let keyword creates variable
+console.log(age, year);
+
+// js reads in  order
+// age is reassigned to 30
+age = 30;
+console.log(age, year);
+
+// let allows variables to be changed/amended
+// const keyword does not allow variables to be reassigned
+const points = 100;
+console.log(points);
+
+// variables must use camelCase
+// variables can only contain letters, numbers, underscores or $ sign
+// variables cannot start with a number
+// variables cannot use JS reserved keywords
+
+// JS data Types
+// Numbers - 1, 2, 3 
+// String - Text in ' ' or " ", numbers in ' ' or " " become strings data type and not number data type
+// Boolean - True or False
+// Null - Explicity set a variable with no value
+// Undefined - Automatically given to variables that have not yet been defined
+// Object - Data Structures - Arrays, Dates, Literals
+// Symbol - Used with Objects
+
+//  Continue from here > https://youtu.be/FhguwBJeqWs?list=PL4cUxeGkcC9haFPT7J25Q9GRB_ZkFrQAc&t=1239
